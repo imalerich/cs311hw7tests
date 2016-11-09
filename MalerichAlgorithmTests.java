99f5d2463e714cfb433303c9c3bb2869cb53ba09
Ian Malerich <ian.malerich@gmail.com>
2016-11-08 22:42:30 -0600

    added tests for all topological sorts

--- MalerichAlgorithmTests.java
+++ MalerichAlgorithmTests.java
@@ -76,4 +76,63 @@ public class MalerichAlgorithmTests {
 		// an exception or has an infinite loop hiding in it.
 		GraphAlgorithms.<String, String>TopologicalSort(test);
 	}
+	
+	@Test public void testAllTopoSort0() {
+		Graph<String, String> test = new Graph<String, String>();
+		test.setDirectedGraph();
+
+		test.addVertex("B");
+		test.addVertex("A");
+		test.addVertex("C");
+		
+		test.addEdge("B", "C");
+		test.addEdge("A", "B");
+		
+		// There should be only one valid topological sort with this input.
+		List<List<Vertex<String>>> res = GraphAlgorithms.AllTopologicalSort(test);
+		assertEquals("There is only 1 valid topological sort.", 1, res.size());
+		List<Vertex<String>> arr = res.get(0);
+		assertEquals("A", arr.get(0).getVertexName());
+		assertEquals("B", arr.get(1).getVertexName());
+		assertEquals("C", arr.get(2).getVertexName());
+	}
+	
+	@Test public void testAllTopoSort1() {
+		Graph<String, String> test = new Graph<String, String>();
+		test.setDirectedGraph();
+
+		test.addVertex("C");
+		test.addVertex("A");
+		test.addVertex("B");
+		
+		test.addEdge("A", "B");
+		test.addEdge("A", "C");
+		
+		// A must be first, but ordering of B, C does not matter, thus there are 2 possible topological sorts.
+		List<List<Vertex<String>>> res = GraphAlgorithms.AllTopologicalSort(test);
+		assertEquals("There is only 2 valid topological sort.", 2, res.size());
+		// Make sure they both start with A.
+		assertEquals("A", res.get(0).get(0).getVertexName());
+		assertEquals("A", res.get(1).get(0).getVertexName());
+	}
+	
+	@Test public void testAllTopoSort2() {
+		Graph<String, String> test = new Graph<String, String>();
+		test.setDirectedGraph();
+		
+		final int COUNT = 10;
+		int fact = 1;
+		for (int i=COUNT; i>1; i--) {
+			fact *= i;
+		}
+
+		// Add 10 vertices and 0 edges.
+		for (int i=0; i<COUNT; i++) {
+			test.addVertex(String.valueOf(i));
+		}
+		
+		// Number of topological sorts is equal to 10!
+		List<List<Vertex<String>>> res = GraphAlgorithms.AllTopologicalSort(test);
+		assertEquals("There are 10! valid topological sort.", fact, res.size());
+	}
 }
